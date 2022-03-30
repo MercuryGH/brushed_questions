11770196a5c5a9e2d6d24d2e246722277e80fa70
Mercury <2818676426@qq.com>
2022-03-31 07:59:58 +0800

    add

---
+++ 1004/1004.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using std::vector, std::string;
+
+/**
+ * DP 时间复杂度 O(nk)，滑动窗口时间复杂度 O(n)
+ * 
+ * 重点：题意转换。把「最多可以把 K 个 0 变成 1，求仅包含 1 的最长子数组的长度」
+ * 转换为 「找出一个最长的子数组，该子数组内最多允许有 K 个 0 」。
+ */
+class Solution
+{
+public:
+    int longestOnes(vector<int> &nums, int k)
+    {
+        const int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        int windowZeroNum = 0;
+        while (true) {
+            if (r >= n) {
+                break;
+            }
+            if (nums[r] == 0) {
+                windowZeroNum++;
+            }
+            if (windowZeroNum > k) {
+                while (true) {
+                    if (nums[l] == 0) {
+                        windowZeroNum--;
+                    }
+                    l++;
+                    if (windowZeroNum == k) {
+                        break;
+                    }
+                }
+            }
+
+            ans = std::max(ans, r - l + 1);
+            r++;
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 0, lsum = 0, rsum = 0;
+        int ans = 0;
+        for (int right = 0; right < n; ++right) {
+            rsum += 1 - nums[right];
+            while (lsum < rsum - k) {
+                lsum += 1 - nums[left];
+                ++left;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
+*/
--- test.cpp
+++ test.cpp
@@ -14,6 +14,76 @@
 #include <vector>
 using std::vector, std::string;
 
+class Solution
+{
+    // 动态规划 O(nk)，超时
+    /*
+    int dpAt(string s, int k, char c)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 0; j <= k; j++)
+            {
+                if (s[i] == c)
+                {
+                    dp[i][j] = dp[i - 1][j] + 1;
+                }
+                else if (j > 0)
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                ans = std::max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+    */
+    int longestCs(string answerKey, int k, char c)
+    {
+        const int n = answerKey.size();
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        int windowZeroNum = 0;
+        while (true)
+        {
+            if (r >= n)
+            {
+                break;
+            }
+            if (answerKey[r] == c)
+            {
+                windowZeroNum++;
+            }
+            if (windowZeroNum > k)
+            {
+                while (true)
+                {
+                    if (answerKey[l] == c)
+                    {
+                        windowZeroNum--;
+                    }
+                    l++;
+                    if (windowZeroNum == k)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            ans = std::max(ans, r - l + 1);
+            r++;
+        }
+        return ans;
+    }
+
+public:
+    int maxConsecutiveAnswers(string answerKey, int k)
+    {
+        return std::max(longestCs(answerKey, k, 'T'), longestCs(answerKey, k, 'F'));
+    }
+};
+
 int main()
 {
     bool b = false;
