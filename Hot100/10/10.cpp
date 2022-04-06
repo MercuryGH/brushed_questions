c179fb6e12a2a70517c2f35df53d30f4198bb667
Mercury <2818676426@qq.com>
2022-04-06 21:09:03 +0800

    add

--- Hot100/10/10.cpp
+++ Hot100/10/10.cpp
@@ -1,22 +1,25 @@
 #include <string>
 #include <regex>
+#include <unordered_set>
 using std::string;
 
 /**
  * 正则表达式匹配
  * 只需支持 . 和 *
  */
-class Solution {
+class Solution
+{
 public:
     /**
      * 调库一行
      */
-    bool isMatch_LIB(string s, string p) {
+    bool isMatch_LIB(string s, string p)
+    {
         return std::regex_match(s, std::regex(p, std::regex::optimize));
     }
 
     /**
-     * 手动 DFA 解法
+     * 手动 FA 解法
      * 代码很长，不写了。详见./nfa/Nfa.java
      * 基础思路与手算 Regex -> NFA 是一致的。
      * 然后
@@ -25,15 +28,63 @@ public:
      * C++ 的 std::regex可以在构造的时候再传一个 std::regex::optimize，可以让NFA优化成DFA（但具体有没有化简状态未知）
      * 如果不传std::regex::optimize，则C++的正则引擎只会生成NFA。
      */
-    bool isMatch_NFA(string s, string p) {
+
+    /**
+     * 模拟 NFA
+     * 每个状态用 int 来唯一确定
+     * s: 待匹配的字符串， p: 正则表达式
+     */
+    bool isMatch_NFA(string s, string p)
+    {
+        const int n = s.length();
+        const int m = p.length();
+        std::unordered_set<int> curStates;
+
+        const auto add = [&](std::unordered_set<int> &states, int state)
+        {
+            states.insert(state);
+            while (state + 1 < m && p[state + 1] == '*') // 这里的判断 <m 只是为了防止越界
+            {
+                state += 2; // 指针向右移动两格（考虑空转移）
+                states.insert(state);
+            }
+        };
+        add(curStates, 0);
+
+        bool matched = false;
+        for (const char c : s)
+        {
+            matched = false;
+            std::unordered_set<int> nextStates;
+            for (const auto &curState : curStates)
+            {
+                if (p[curState] == c || p[curState] == '.')
+                {
+                    if (curState + 1 < m && p[curState + 1] == '*') // 空转移
+                    {
+                        add(nextStates, curState);
+                    }
+                    else // 非空转移
+                    {
+                        add(nextStates, curState + 1);
+                    }
+                }
+            }
+            if (nextStates.find(m) != nextStates.end())
+            {
+                matched = true;
+            }
+            curStates = nextStates;
+        }
+        return matched;
     }
 
     /**
      * DP算法
-     * 
+     *
      * 太复杂一点也不优雅，不写了
      */
-    bool isMatch_DP(string s, string p) {
-
+    bool isMatch_DP(string s, string p)
+    {
     }
 };
--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -14,48 +14,157 @@
 #include <vector>
 using std::vector, std::string;
 
+struct Operator;
+struct Number;
+struct Bracket;
+
 struct Token
 {
-    virtual void modifyStack(std::stack<int> &s) const = 0;
+    virtual void performInConvertion(vector<Token *> &result, std::stack<Operator *> &s) = 0;
+    virtual void performInEvaluaion(std::stack<Number *> &s) = 0;
     virtual void print() const = 0;
 };
+
 struct Number : Token
 {
     int value;
     Number(int value) : value(value) {}
 
-    virtual void modifyStack(std::stack<int> &s) const override
+    // 数，直接输出
+    virtual void performInConvertion(vector<Token *> &result, std::stack<Operator *> &s) override
     {
+        result.push_back(this);
+    }
+    virtual void performInEvaluaion(std::stack<Number *> &s) override
+    {
+        s.push(this);
     }
     virtual void print() const override
     {
         std::cout << value << "\n";
     }
 };
+
 struct Operator : Token
 {
     char op;
     Operator(char op) : op(op) {}
 
-    virtual void modifyStack(std::stack<int> &s) const override
+    // 维护单调栈
+    // 栈顶的运算符优先级高
+    virtual void performInConvertion(vector<Token *> &result, std::stack<Operator *> &s) override
     {
+        while (true)
+        {
+            if (s.empty() == true)
+            {
+                break;
+            }
+            Operator *top = s.top();
+            if (top->op == '(') // 与 s.empty() 等价
+            {
+                break;
+            }
+            if (top->getPriority() < this->getPriority())
+            {
+                break;
+            }
+
+            s.pop();
+            result.push_back(top);
+        }
+        s.push(this);
+    }
+    virtual void performInEvaluaion(std::stack<Number *> &s) override
+    {
+        Number *a1 = s.top();
+        s.pop();
+        if (s.empty() == true && op == '-') // 一操作数。实际上根本不可能走这一分支
+        {
+            a1->value = -a1->value;
+            s.push(a1);
+        }
+        else // 二操作数
+        // 实际上，以下操作都可以简化为只 pop 一次，效率更高
+        {
+            Number *a2 = s.top();
+            s.pop();
+
+            Number *res = nullptr;
+            switch (op)
+            {
+            case '+':
+                res = new Number(a2->value + a1->value);
+                break;
+            case '-':
+                res = new Number(a2->value - a1->value);
+                break;
+            case '*':
+                res = new Number(a2->value * a1->value);
+                break;
+            case '/':
+                res = new Number(a2->value / a1->value);
+                break;
+            default:
+                break;
+            }
+            delete a1;
+            delete a2;
+            s.push(res);
+        }
     }
     virtual void print() const override
     {
         std::cout << op << "\n";
     }
+    int getPriority() const
+    {
+        if (op == '+' || op == '-')
+        {
+            return 1;
+        }
+        else if (op == '*' || op == '/')
+        {
+            return 2;
+        }
+        return 0;
+    }
 };
-struct Bracket : Token
-{
-    char bracket;
-    Bracket(char bracket) : bracket(bracket) {}
 
-    virtual void modifyStack(std::stack<int> &s) const override
+struct Bracket : Operator
+{
+    Bracket(char op) : Operator(op) {}
+
+    virtual void performInConvertion(vector<Token *> &result, std::stack<Operator *> &s) override
+    {
+        if (op == '(')
+        {
+            s.push(this);
+        }
+        else if (op == ')')
+        {
+            // 右括号，pop直到 ( 被弹出
+            // 如果栈为空，也没有 (，说明括号不匹配（未处理此情况）
+            while (s.empty() == false)
+            {
+                Operator *top = s.top();
+                s.pop();
+                if (top->op == '(')
+                {
+                    break;
+                }
+                result.push_back(top);
+            }
+        }
+    }
+
+    // 后缀表达式不含括号，不用实现
+    virtual void performInEvaluaion(std::stack<Number *> &s) override
     {
     }
     virtual void print() const override
     {
-        std::cout << bracket << "\n";
+        std::cout << op << "\n";
     }
 };
 
@@ -65,10 +174,8 @@ public:
     int calculate(string s)
     {
         vector<Token *> tokens = lex(s);
-        for (const auto token : tokens) {
-            token->print();
-        }
-        return 0;
+        vector<Token *> postFix = convert2PostFix(tokens);
+        return getResultFromPostFix(postFix);
     }
 
 private:
@@ -81,6 +188,7 @@ private:
         return c == '(' || c == ')';
     }
 
+    // 词法分析（轻量级DFA）
     vector<Token *> lex(string s)
     {
         int n = s.length();
@@ -110,6 +218,23 @@ private:
                 }
                 else if (isOperator(c))
                 {
+                    // 不太优雅：为了避免将 - 当作一元运算符解释，在一元的 - 前面插入一个 0
+                    if (c == '-')
+                    {
+                        if (tokens.empty() == true)
+                        {
+                            tokens.push_back(new Number(0));
+                        }
+                        else
+                        {
+                            // 回头看一步
+                            Operator *top = dynamic_cast<Operator *>(tokens.back());
+                            if (top != nullptr && top->op == '(')
+                            {
+                                tokens.push_back(new Number(0));
+                            }
+                        }
+                    }
                     tokens.push_back(new Operator(c));
                 }
             }
@@ -121,27 +246,69 @@ private:
         return tokens;
     }
 
-    int calculate(vector<Token *> tokens, int start, int end)
+    // 中缀 token 转换为 后缀 token
+    vector<Token *> convert2PostFix(const vector<Token *> &inFix)
     {
-        std::stack<int> s;
-        bool positive = false;
-        int index = start;
-        while (true)
-        {
-            if (index > end)
-            {
-                break;
-            }
+        std::stack<Operator *> s;
+        vector<Token *> result;
 
-            Token *curToken = tokens[index];
-            curToken->modifyStack(s);
-            index++;
+        for (const auto token : inFix)
+        {
+            token->performInConvertion(result, s);
         }
+
+        // 把剩下的表达式也放回去
+        while (s.empty() == false)
+        {
+            result.push_back(s.top());
+            s.pop();
+        }
+
+        return result;
+    }
+
+    // 后缀token直接求值
+    int getResultFromPostFix(const vector<Token *> &postFix)
+    {
+        std::stack<Number *> s;
+
+        for (const auto token : postFix)
+        {
+            token->performInEvaluaion(s);
+        }
+
+        return s.top()->value;
+    }
+
+    vector<Token *> getPostFixTokensFromStringVector(vector<string> &tokensString)
+    {
+        vector<Token *> tokens;
+        for (const string s : tokensString)
+        {
+            if (s.length() == 1 && isOperator(s[0]))
+            {
+                tokens.push_back(new Operator(s[0]));
+            }
+            else
+            {
+                tokens.push_back(new Number(std::stoi(s)));
+            }
+        }
+        return tokens;
+    }
+
+public:
+    int evalRPN(vector<string> &tokens)
+    {
+        vector<Token *> postFix = getPostFixTokensFromStringVector(tokens);
+        return getResultFromPostFix(postFix);
     }
 };
 
-int main() {
+int main()
+{
     Solution s;
-    s.calculate("1 + 2 - 3 * (4 + 5)");
+    int ans = s.calculate("-1 + (-4 + 5)");
+    std::cout << ans << "\n";
     return 0;
 }
